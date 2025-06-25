ea17bb9b85ef3d58848cd3574f486998c868eb15
Lu1sGustavo <tnisdlex@gmail.com>
2025-06-25 11:24:19 -0300

    organização e correção do código

--- scripts/calculadora.js
+++ scripts/calculadora.js
@@ -1,6 +1,7 @@
-function somar(somar) {
+function somar() {
     let num1 = document.getElementById("num1").value;
     let num2 = document.getElementById("num2").value;
+    let soma = parseFloat(num1) + parseFloat(num2);
     document.getElementById("resultado").innerHTML =
-    "Resultado: " = somar;
+    "Resultado: " + soma;
 }
